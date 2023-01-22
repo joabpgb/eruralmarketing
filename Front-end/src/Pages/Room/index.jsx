9740da188faa36fc3669cff1bb02c87be2cf07c3
Joab <joabpgb7@gmail.com>
2023-01-21 23:08:29 -0300

    feat: creating room with video

---
+++ Front-end/src/Pages/Room/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../Components/Header'
+import YoutubeEmbed from '../../Components/Video'
+
+export default function Room(){
+    return (
+        <div>
+            <Header></Header>
+            <YoutubeEmbed embedId='WsEv01p3GXU'></YoutubeEmbed>
+        </div>
+    )
+}
