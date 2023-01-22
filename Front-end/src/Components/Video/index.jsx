11c4e1c56ebd88abc6dd082c740000128aa74e87
Joab <joabpgb7@gmail.com>
2023-01-21 23:07:51 -0300

    feat: incorporate video component

---
+++ Front-end/src/Components/Video/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './index.css'
+import PropTypes from "prop-types";
+
+const YoutubeEmbed = ({ embedId }) => (
+    <div className="video-responsive">
+      <iframe
+        width="80%"
+        height="720"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
+  
+  YoutubeEmbed.propTypes = {
+    embedId: PropTypes.string.isRequired
+  };
+  
+  export default YoutubeEmbed;
