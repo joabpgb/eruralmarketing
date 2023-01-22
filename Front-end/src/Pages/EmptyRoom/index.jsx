302aff0da831ff51575d1c3d3afe0b770b52c2d7
Joab <joabpgb7@gmail.com>
2023-01-21 23:04:09 -0300

    feat: empty room

---
+++ Front-end/src/Pages/EmptyRoom/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../Components/Header'
+import Emptyroom from '../../Components/EmptyRoom'
+
+export default function EmptyRoom(){
+    return (
+        <div>
+            <Header></Header>
+            <Emptyroom></Emptyroom>
+        </div>
+    )
+}
