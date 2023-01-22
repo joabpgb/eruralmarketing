2e6528be1e7abb0d2b9805e361181eb28a3dfd01
Joab <joabpgb7@gmail.com>
2023-01-21 23:08:58 -0300

    feat: app returning all the screens

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,11 +1,18 @@
 import './index.css'
 import Home from "./Pages/Home";
+import Room from "./Pages/EmptyRoom";
+import Video from "./Pages/Room"
+import {BrowserRouter,Switch,Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/room" component={Room}/>
+        <Route path="/video" component={Video}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
