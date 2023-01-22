bb74a8b67941b9a56cb82a4501f5c0f0fcb61c5b
Joab <joabpgb7@gmail.com>
2023-01-21 22:59:57 -0300

    feat: linking homepage with emptyRoom page

--- Front-end/src/Components/Home Body/index.jsx
+++ Front-end/src/Components/Home Body/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Ox from "../../Assets/Ox.png"
 import './index.css'
-
+import {useHistory} from "react-router-dom";
 
 export default function Homebody(){
+    const history = useHistory()
+    function handleGoRoom() {
+        window.location.href="http://localhost:3000/room"
+        }
     return (
         <div className='homeBody'>
             <div className='heroText'>
                 <h2>Olá, vendedor</h2>
                 <h4>Crie uma sala para disponibilizar um vídeo sobre o produto a ser vendido para seus potenciais clientes.</h4>
-                <button>Criar sala</button>
+                <button onClick={handleGoRoom} className='createRoom'>Criar sala</button>
             </div>
             <img src={Ox} alt="Ox"/>
         </div>
